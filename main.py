1d0d304a8ebfe66c9954e9b9306175062dae0fba
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2017-03-02 12:38:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
+import sys
+sys.path.insert(0,'./KeyHandlers')
+from KeyListener import KeyListener
+from HandleClass import Handler
 import pyperclip
-import json
+import pyautogui
+from pynput.keyboard import Key, Controller
+sys.path.insert(0,'./Ui')
+from lookupBox import Ui_Form
+from PyQt4 import QtGui
 
-dic = open("dictionary.json", 'r')
-parsed = json.loads(dic.read())
+class DictionBox(QtGui.QWidget,Ui_Form):
+	def __init__(self,parent=None):
+		QtGui.QWidget.__init__(self,parent)
+		text = pyperclip.paste()
+		self.setupUi(self,text)
+		
 
-x = pyperclip.paste().upper()
-print parsed[x]
+
+def StartCopy():
+	import sys
+	keyboard = Controller()
+	with keyboard.pressed(Key.ctrl):
+		keyboard.press('c')
+		keyboard.release('c')
+	
+	app=QtGui.QApplication(sys.argv)
+	window=DictionBox()
+	window.show()
+	app.quit()
+	app.exec_()
+	while True:
+		keylistener = KeyListener()
+		keylistener.addKeyListener("L_CTRL+q", StartCopy)
+		handle = Handler(keylistener)
+		
+	
+	
+def hello():
+	print 'you beautiful thing'
+
+keylistener = KeyListener()
+keylistener.addKeyListener("L_CTRL+q", StartCopy)
+handle = Handler(keylistener)
+
