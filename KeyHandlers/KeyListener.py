f043aba7fcc48462aeae8e5c1ab3f3e2074fd0db
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2017-03-02 12:41:50 +0530

    Add files via upload

---
+++ KeyHandlers/KeyListener.py
@@ -0,0 +1,28 @@
+class KeyListener(object):
+    def __init__(self):
+        
+        
+        self.pressedKeys = []
+        self.listeners = {}
+
+    def press(self, character):
+        
+        self.pressedKeys.append(character)
+        action = self.listeners.get(tuple(self.pressedKeys), False)
+        if action:
+            action()
+
+    def release(self, character):
+        if character in self.pressedKeys:
+            self.pressedKeys.remove(character)
+
+    def addKeyListener(self, hotkeys, callable):
+        keys = tuple(hotkeys.split("+"))
+        self.listeners[keys] = callable
+
+
+
+
+
+
+
